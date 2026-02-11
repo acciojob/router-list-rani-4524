7b22c1d79214cb3db3d1355f3db2df2cf93c2bf5
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-11 15:43:55 +0530

    Update ItemDetails.jsx

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-const ItemDetails = ({items}) => {
+const items = [
+  { id: 1, name: "Item 1", description: "Description for Item 1" },
+  { id: 2, name: "Item 2", description: "Description for Item 2" },
+  { id: 3, name: "Item 3", description: "Description for Item 3" }
+];
+
+const ItemDetails = () => {
   const {id} = useParams()
 
   const item = items.find((item) => item.id === parseInt(id));
