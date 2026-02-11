4ec2ef5e2c527eadd64541a71e58cc0e9622638c
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-11 15:44:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,14 @@ import ItemDetails from "./ItemDetails";
 
 const App = () => {
 
-  const items = [
-    { id: 1, name: "Item 1", description: "Description for Item 1" },
-    { id: 2, name: "Item 2", description: "Description for Item 2" },
-    { id: 3, name: "Item 3", description: "Description for Item 3" }
-  ];
-
   return (
     <div>
       {/* Do not remove the main div */}
      
      <BrowserRouter>
       <Switch >
-          <Route path="/" element={<ItemList  items={items}/>} />
-          <Route path="/items/:id" element={<ItemDetails items={items}/>} />
+          <Route path="/" element={<ItemList />} />
+          <Route path="/items/:id" element={<ItemDetails/>} />
         </Switch >
     </BrowserRouter>
     </div>
