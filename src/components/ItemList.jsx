d8415e7f9df8d2de6150dd5609ef330d3957ebf0
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-11 15:44:17 +0530

    Update ItemList.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const ItemList = ({ items}) => {
+const items = [
+  { id: 1, name: "Item 1", description: "Description for Item 1" },
+  { id: 2, name: "Item 2", description: "Description for Item 2" },
+  { id: 3, name: "Item 3", description: "Description for Item 3" }
+];
+const ItemList = () => {
   return (
     <div>
       <h1>Item List</h1>
